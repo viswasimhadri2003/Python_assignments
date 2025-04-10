0a78fdfe5d8a6df759532d5f67c552c64a1a4190
viswasimhadri2003 <142866306+viswasimhadri2003@users.noreply.github.com>
2025-04-10 09:08:02 +0530

    Day4_assignment_2.py

--- Day4_assignment_2.py
+++ Day4_assignment_2.py
@@ -2,21 +2,21 @@ import os
 import datetime
 
 class File:
-    def __init__(self, directory):
-        self.directory = directory
+    def __init__(self, path):
+        self.path = path
 
     def getMaxSizeFile(self, n):     
-        files = [(file, os.path.getsize(os.path.join(self.directory, file))) 
-                 for file in os.listdir(self.directory) if os.path.isfile(os.path.join(self.directory, file))]
+        files = [(file, os.path.getsize(os.path.join(self.path, file))) 
+                 for file in os.listdir(self.path) if os.path.isfile(os.path.join(self.path, file))]
          
         files.sort(key=lambda x: x[1] ,reverse=True)  # Sort files by size in descending order
         return [file[0] for file in files[:n]]    #get the top n and also only the file name
 
     def getLatestFiles(self, date):
-        files = [file for file in os.listdir(self.directory) if os.path.isfile(os.path.join(self.directory, file))]
+        files = [file for file in os.listdir(self.path) if os.path.isfile(os.path.join(self.path, file))]
         latest_files = []    #creating a list
         for file in files:
-            modification_time = datetime.datetime.fromtimestamp(os.path.getmtime(os.path.join(self.directory, file)))
+            modification_time = datetime.datetime.fromtimestamp(os.path.getmtime(os.path.join(self.path, file)))
             if modification_time > date:
                 latest_files.append(file)
         return latest_files
